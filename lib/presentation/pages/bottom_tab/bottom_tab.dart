da342f8e88258952e2a786aaf05bca6ce4af6787
sahinefe35 <efe.mate@gmail.com>
2022-11-16 13:41:46 +0300

    refactor the bottom_tab folder codes

--- lib/presentation/pages/bottom_tab/bottom_tab.dart
+++ lib/presentation/pages/bottom_tab/bottom_tab.dart
@@ -6,6 +6,7 @@ import 'package:flutter_production_app/application/auth/auth_cubit.dart';
 import 'package:flutter_production_app/application/chat/chat_setup/chat_setup_cubit.dart';
 
 import 'package:flutter_production_app/injection.dart';
+import 'package:flutter_production_app/presentation/pages/bottom_tab/widgets/bottom_navigation_builder.dart';
 import 'package:flutter_production_app/presentation/routes/router.gr.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
@@ -78,26 +79,7 @@ class _BottomTabPageState extends State<BottomTabPage> {
                 const GroupsRoute(),
                 const ProfileRoute(),
               ],
-              bottomNavigationBuilder: (_, tabsRouter) {
-                return BottomNavigationBar(
-                  currentIndex: tabsRouter.activeIndex,
-                  onTap: tabsRouter.setActiveIndex,
-                  items: const [
-                    BottomNavigationBarItem(
-                      label: 'Chats',
-                      icon: Icon(CupertinoIcons.chat_bubble_2),
-                    ),
-                    BottomNavigationBarItem(
-                      label: 'Groups',
-                      icon: Icon(CupertinoIcons.person_3),
-                    ),
-                    BottomNavigationBarItem(
-                      label: 'Profile',
-                      icon: Icon(CupertinoIcons.person),
-                    ),
-                  ],
-                );
-              },
+              bottomNavigationBuilder: bottomNavigationBuilder,
             ),
           ),
         ),
