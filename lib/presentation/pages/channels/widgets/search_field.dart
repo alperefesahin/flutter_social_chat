cc2bb1b7733f676ce219df1f7bcc896a257f0fa0
sahinefe35 <efe.mate@gmail.com>
2022-12-08 19:03:54 +0300

    disable autocorrect parameter

--- lib/presentation/pages/channels/widgets/search_field.dart
+++ lib/presentation/pages/channels/widgets/search_field.dart
@@ -14,6 +14,7 @@ class SearchField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(15),
       child: TextField(
+        autocorrect: false,
         cursorColor: kiwiColor,
         onChanged: onChanged,
         decoration: const InputDecoration(
