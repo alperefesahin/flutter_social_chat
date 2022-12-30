42e8ffbf81d58701887abd31dd05628c2d8a0ac9
sahinefe35 <efe.mate@gmail.com>
2022-12-30 15:44:37 +0300

    implement the 1-1 chat ui

--- lib/presentation/pages/create_new_chat/widgets/create_new_chat_button.dart
+++ lib/presentation/pages/create_new_chat/widgets/create_new_chat_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_production_app/application/chat/chat_management/chat_management_cubit.dart';
+import 'package:flutter_production_app/presentation/common_widgets/colors.dart';
 import 'package:flutter_production_app/presentation/pages/create_new_chat/constants/texts.dart';
 
 class CreateNewChatButton extends StatelessWidget {
@@ -13,7 +14,11 @@ class CreateNewChatButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
+    return ElevatedButton(
+      style: const ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(customIndigoColor),
+        padding: MaterialStatePropertyAll(EdgeInsets.all(15)),
+      ),
       onPressed: () {
         context.read<ChatManagementCubit>().createNewChannel(
               isCreateNewChatPageForCreatingGroup: isCreateNewChatPageForCreatingGroup,
